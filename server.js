f901031c0eb6ad2fe9a59f5bc9600c654bc79d1f
nandalalainfotech <nandalalainfotech@gmail.com>
2022-07-12 12:56:03 +0530

    update done by shameem

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ import express from 'express';
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static('./dist/sirius-matrimony-frontend'));
+app.use(express.static('./dist/heroku-demo-trail'));
 app.get('/*', function (req, res) {
-    res.sendFile('index.html', { root: 'dist/sirius-matrimony-frontend/' });
+    res.sendFile('index.html', { root: 'dist/heroku-demo-trail/' });
 });
 app.listen(process.env.PORT || 8080);
 
