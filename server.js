040ecd433f9eddb698be2a1fd97fa892e53c1a16
nandalalainfotech <nandalalainfotech@gmail.com>
2022-07-12 12:16:42 +0530

    changes are done.

--- server.js
+++ server.js
@@ -3,13 +3,10 @@ import express from 'express';
 
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
 app.use(express.static('./dist/sirius-matrimony-frontend'));
 app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: 'dist/sirius-matrimony-frontend/'});
+    res.sendFile('index.html', { root: 'dist/sirius-matrimony-frontend/' });
 });
-
 app.listen(process.env.PORT || 8080);
 
